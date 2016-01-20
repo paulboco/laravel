ac5674bbf2478da2ff29d7f3d2c7be7ff036c9e5
paulboco <paulboco@cableone.net>
2016-01-19 20:25:53 -0600

    Fix bug in route.php.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,13 +1,13 @@
 <?php
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::group(['middleware' => 'web'], function () {
     Route::auth();
 
     Route::group(['middleware' => 'auth'], function () {
         Route::get('/home', 'HomeController@index');
+    });
+
+    Route::get('/', function () {
+        return view('welcome');
     });
 });
